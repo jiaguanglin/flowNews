e95f3555a855e21794a323bbaf3b1ca828b9b637
jiaguanglin <myelin9166@foxmail.com>
2025-03-10 16:54:51 +0800

    Update wbHots.py

--- wbHots.py
+++ wbHots.py
@@ -122,7 +122,7 @@ def weixin_push(media_id):
 
 if __name__ == '__main__':
     time_name = datetime.now().strftime('20%y年%m月%d日%H')
-    webHot()
+    # webHot()
     # wxbootkey = '61858489-6390-4dc8-8e1f-f58536d9fc35'
     wxbootkey = '38f8e08a-3f62-410e-9f84-b60799673f69'
-    # weixin_push(upload_image(time_name))
+    weixin_push(upload_image(time_name))
