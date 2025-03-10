cd05aff872bab7735210d59d693212be2dc0582c
jiaguanglin <myelin9166@foxmail.com>
2025-03-10 16:50:56 +0800

    Update wbHots.py

--- wbHots.py
+++ wbHots.py
@@ -109,7 +109,7 @@ def weixin_push(media_id):
             "msgtype":"image",
             "touser":"@all",
             "image":{
-                    "media_id":media_id
+                    "media_id": media_id
             }
         }
     headers = {'Content-Type': 'application/json'}
@@ -125,4 +125,4 @@ if __name__ == '__main__':
     webHot()
     # wxbootkey = '61858489-6390-4dc8-8e1f-f58536d9fc35'
     wxbootkey = '38f8e08a-3f62-410e-9f84-b60799673f69'
-    weixin_push(upload_image(time_name))
+    # weixin_push(upload_image(time_name))
